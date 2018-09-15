4a19873729d0feab7bcbfd894da88f98480dd380
IbrahimSharif <iba.sharif@icloud.com>
2018-09-15 11:03:52 +0200

    Vuetify ADD* Router and compenonts initial

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,63 @@
 <template>
+
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <head>
+      <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+    </head>
+    <v-app id="inspire" dark>
+      <v-navigation-drawer
+        clipped
+        fixed
+        v-model="drawer"
+        app
+      >
+        <v-list dense>
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Dashboard</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>settings</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Place AED</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app fixed clipped-left>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout justify-center align-center>
+            <v-flex shrink>
+              <router-view></router-view>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <v-footer app fixed>
+        <span>&copy; 2017</span>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data: () => ({
+    drawer: true,
+  }),
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import CreateAED from '@/components/Create-AED';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/Create',
       name: 'HelloWorld',
-      component: HelloWorld,
+      component: CreateAED,
     },
   ],
 });
